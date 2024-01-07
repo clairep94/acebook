cc12728fa428f19e46dbf32c5ac01f20dfcdf289
Claire Peng <128436909+clairep94@users.noreply.github.com>
2024-01-07 13:06:23 +0000

    Adds WIP Friends Page

--- frontend/src/components/app/ProtectedRoutes2.js
+++ frontend/src/components/app/ProtectedRoutes2.js
@@ -8,17 +8,18 @@ import { isLoggedIn } from '../../utilities/LoggedInCheck';
 import LoginPopup from '../auth/LoginPopup';
 import { findUserFullProfile } from '../../api_calls/usersAPI';
 import {io} from 'socket.io-client';
-// import { fetchChats } from "../../api_calls/chatsAPI";
-// import ChatsList from '../messaging/ChatsList';
 
-// import Profile from '../profilepage/Profile';
-// import OwnProfile from '../profilepage/OwnProfile';
+// import { fetchChats } from "../../api_calls/chatsAPI";
+
 import Navbar from '../navbar/Navbar';
 import getSessionUserID from '../../utilities/GetSessionUserID';
-// import Feed from '../feed/Feed';
-// import ChatWindow from '../messaging/ChatWindow';
 
 import ProfilePage from '../../pages/ProfilePage'
+import FriendsPage from '../../pages/FriendsPage';
+
+
+
+
 const ProtectedRoutes = ({navigate}) => {
 
 
@@ -79,6 +80,12 @@ const ProtectedRoutes = ({navigate}) => {
         <Route path="/users/:userID"  element={ 
             <ProfilePage navigate={navigate} token={token} setToken={setToken} 
           sessionUserID={sessionUserID} sessionUser={sessionUser} setSessionUser={setSessionUser}/>}/>
+
+        {/* ------  FRIENDS PAGE ------  */}
+        <Route path="/friends"  element={ 
+            <FriendsPage />}/>
+
+
 
       </Routes>
 
--- frontend/src/pages/FriendsPage.jsx
+++ frontend/src/pages/FriendsPage.jsx
@@ -1,66 +1,31 @@
 import React, {useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom';
-import { findUser } from '../api_calls/usersAPI';
 
 
 export default function FriendsPage({ navigate, token, setToken, sessionUserID, sessionUser, setSessionUser }) {
 
-  // =========== STATE VARIABLES ==========================
-  // PROFILE PAGE OWNER:
-  const { userID } = useParams(); //ID of the profile page owner
-  const targetID = userID; // renamed variable for clarity
-  const [target, setTarget] = useState(null); // State to hold target data
-
-  const [section, setSection] = useState('Posts'); // holds what view the user is current on, default is Posts
-
-  // ========= COMPONENT MOUNT: Set Profile Owner Data & Session User Data ===============
-  // Profile owner
-  useEffect(() => {
-    if (token) {
-      if (sessionUserID === targetID) {
-        navigate('/profile')
-      }
-      findUser(token, targetID)
-      .then(targetData => {
-        window.localStorage.setItem("token", targetData.token);
-        setToken(window.localStorage.getItem("token"));
-        setTarget(targetData.user);
-        console.log("Profile Owner ", targetData.user)
-      })
-    }
-  }, []);
 
 
   // ======================== JSX FOR COMPONENT =============================================
-  if (target && sessionUser) {
-    return (
-      <div className='w-full overflow-scroll bg-green-100 mb-2 '>
-        {/* MAIN PAGE */}
 
-        {/*  ================= HEADER SECTION ================= */}
-        <div aria-label='header container' 
-          className='w-full bg-white h-[33.5rem] lg:h-[36.5rem] xl:h-[39.5rem] flex flex-col items-center
-          shadow-md
-          '>
-          <ProfileHeader target={target} setTarget={setTarget} 
-            sessionUserID={sessionUserID} sessionUser={sessionUser} setSessionUser={setSessionUser}
-            section={section} setSection={setSection} token={token} setToken={setToken}
-          />
+    return (
+      <div className='w-full flex bg-green-100 mt-3 mb-2 '>
+        {/* MENU - no overflow? */}
+        <div className='w-[20rem] shadow-lg bg-red-200 flex flex-col'>
+          <p>Friends</p>
+          <p>Friend Requests</p>
         </div>
 
-        {/*  ================= BOTTOM SECTION -- depends on page section: Main or Friends ================= */}
-        <div aria-label='profile main section'
-        className='w-full overflow-auto bg-blue-200 max-w-[100rem] m-auto p-4'
-        >
-        {/* LENGTH TESTER */}
-        <div className='w-[50px] h-[100rem] bg-green-100'></div>
-        <div className='w-[60px] h-5 bg-purple-200'></div>
-          
+        {/* MAIN - overflow scroll */}
+        <div className='bg-yellow-200 overflow-scroll w-full'>
+                {/* LENGTH TESTER */}
+                  <div className='w-[30px] h-[600rem] bg-green-500'></div>
+                  <div className='w-[60px] h-5 bg-purple-200'></div>
+
         </div>
 
         
       </div>
     )
-    }
+  
 
 }
