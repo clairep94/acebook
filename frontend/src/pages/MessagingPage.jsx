b49a0387f420acf7f2248eb221a08e57f62abd31
Claire Peng <128436909+clairep94@users.noreply.github.com>
2024-01-08 09:25:22 +0000

    adds messages box

--- frontend/src/pages/MessagingPage.jsx
+++ frontend/src/pages/MessagingPage.jsx
@@ -5,6 +5,13 @@ import ChatSearchBar from '../components/messaging/chat_searchbar/ChatSearchBar'
 import ChatsMenu from '../components/messaging/ChatsMenu';
 
 
+import ChatCard from '../components/messaging/ChatCard';
+import InputEmoji from 'react-input-emoji';
+import ProfilePicture from '../components/user/ProfilePicture';
+import { BsPersonCircle } from "react-icons/bs";
+import { fetchMessages, sendMessage } from '../api_calls/messagesAPI';
+import MessageCard from '../components/messaging/MessageCard';
+
 export default function MessagingPage({ navigate, token, setToken, sessionUserID, sessionUser, setSessionUser }) {
 
   // ============================= SESSION USER & TOKEN =========================================
@@ -13,6 +20,7 @@ export default function MessagingPage({ navigate, token, setToken, sessionUserID
   // ============================== CHATS & CURRENT CHAT =============================================
   const [chats, setChats] = useState([]);
   const [currentChat, setCurrentChat] = useState(null);
+
 
   // ---------- COMPONENT MOUNT: Get SessionUser's chats ------------
   useEffect(() => {
@@ -57,6 +65,29 @@ export default function MessagingPage({ navigate, token, setToken, sessionUserID
   const checkOnlineStatus = (chat) => {return true}
 
 
+
+  // ========= COMPONENT MOUNT: Set Current Chat's messages ===============
+  useEffect(() => {
+    if (token && currentChat) {
+      fetchMessages(token, currentChat)
+      .then(messagesData => {
+        window.localStorage.setItem("token", messagesData.token)
+        setToken(window.localStorage.getItem("token"))
+
+        setMessages(messagesData.allMessages);
+      })
+    }
+  }, [currentChat]);
+
+  
+  // MOVE TO CHAT WINDOW:
+  // Loading messages, sending messages, receiving messages:
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+
+
+
   // =============================== JSX FOR COMPONENT ===============================================================
 
   return (
@@ -72,43 +103,63 @@ export default function MessagingPage({ navigate, token, setToken, sessionUserID
       </div>
     
     {/* ===================== RIGHT CHAT WINDOW ============================== */}
-    {currentChat ? (<>
+    {currentChat ? (
+    <>
       {/* ============ CHAT SELECTED ======================= */}
-      <div className='w-full flex flex-col bg-green-50'>
-        {/* HEADER */}
-        <div className='bg-white h-[6rem] p-4 pl-5 shadow-[0px_0px_7px_0px_#d9deed]
-        flex flex-row items-center'>
-          <div className='w-[4rem] h-[4rem] bg-zinc-300 rounded-full mr-3'>
-                dot if online
+      <div className='w-full flex flex-col bg-white'>
+        {/*  --------- HEADER ---------------- */}
+        <div className='bg-white h-[6rem] p-4 pl-5 pr-5 shadow-[0px_0px_7px_0px_#d9deed] flex flex-row items-center justify-between'>
+
+          {/* LEFT SIDE CONTAINER */}
+          <div className='flex flex-row items-center'>
+            {/* PROFILE PICTURE */}
+            <div className='w-[4rem] h-[4rem] mr-3 relative'>
+              {/* ONLINE DOT */}
+              {checkOnlineStatus(currentChat) && <div className='bg-lime-400 w-5 h-5 rounded-full absolute left-12 bottom-0 border-white border-solid border-[0.2rem]'/>}
+              {/* PROFILE PICTURE */}
+              <ProfilePicture id={currentChat.members[1]._id} name={currentChat.members[1].firstName + ` ` + currentChat.members[1].lastName}/>
+            </div>
+
+            {/* NAME & ONLINE STATUS */}
+            <div aria-label='partner and online status' className='translate-y-1'>
+              <h4 className='font-semibold text-[1.2rem] '>
+                {currentChat.members[1].firstName} {currentChat.members[1].lastName}
+              </h4>
+              <p className='text-[#8a8a8a] text-sm -translate-y-1'>
+                Active Now
+              </p>
+            </div>
           </div>
-          <div aria-label='partner and online status' className='translate-y-1'>
-            <h4 className='font-semibold text-[1.2rem] '>
-              {currentChat.members[1].firstName} {currentChat.members[1].lastName}
-            </h4>
-            <p className='text-[#8a8a8a] text-sm -translate-y-1'>
-              Active Now
-            </p>
+
+          {/* RIGHT SIDE: GO TO PROFILE ICON */}
+          <div className='group relative hidden sm:block'>
+            <div className='rounded-full text-[2.5rem] text-[#5acad2] hover:bg-slate-100 h-[3.5rem] w-[3.5rem] text-center p-[0.5rem] mr-4'
+              onClick={() => {navigate(`/users/${currentChat.members[1]._id}`)}}>
+              <BsPersonCircle />
+            </div>
+            <div className='hidden group-hover:block absolute w-[6rem] text-sm font-light bg-black/60 text-white p-2 rounded-md 
+            translate-y-1 -translate-x-5 z-50'>
+              Go to profile
+            </div>
           </div>
+
         </div>
 
-        {/* MESSAGES */}
-        <div className='flex flex-grow flex-col bg-red-50 w-full overflow-scroll px-5'>
-          HOW DO I DO THIS PART
-          <div className='h-[2rem]'>Message 1</div>
-          <div className='h-[2rem]'>Message 1</div>
-          <div className='h-[2rem]'>Message 1</div>
-                  <div className='h-[2rem]'>Message 1</div>
-          <div className='h-[2rem]'>Message 1</div>
-          <div className='h-[2rem]'>Message 1</div>
-          <div className='h-[2rem]'>Message 1</div>
-
+        {/* ----------------- MESSAGES ----------------------- */}
+        <div className='flex flex-grow flex-col w-full overflow-scroll px-5 mt-5'>
+          {currentChat && messages && (
+            messages.map((message) => (
+                <MessageCard key={message._id} message={message} sessionUserID={sessionUserID} />
+              ))
+          )}
         </div>
 
         {/* INPUT FIELD */}
-        <div className='flex flex-row bg-green-200 h-[5rem] min-h-[5rem] items-center px-4'>
-          INPUT
+        <div className='flex flex-row h-[6rem] min-h-[6rem] items-center px-4'>
+          <InputEmoji placeholder='Aa'/>
         </div>
       </div>
+
     </>
     ):(
       <>
